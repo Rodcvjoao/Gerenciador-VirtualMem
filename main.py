afc4fdef589bfe846a44221983e7b5e074a47788
Guedxx <pedrogg@id.uff.br>
2025-06-21 16:51:41 -0300

    fix: mains

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
-import tkinter as t
-from pagemanager import PageManager
-from ui_pagina_inicial import Tela_Inicial
-from ui_pagina_configurar import Tela_Configurar
-from ui_pagina_input import Tela_Input
-from ui_pagina_simular import Tela_Simular
+from classesProcessos import *
+from memoriaPrincipal import *
 
-class App(t.Tk): # Herda de tk.Tk
+# "With" é uma palavra reservada que garante o gerenciamento de recursos,
+# abrindo e fechando o arquivo de uma vez só
+with open("caso_de_teste.txt", "r", encoding="utf-8") as f:
+    # Em python, podemos criar tuplas de vários tamanhos e com itens de vários tipos diferentes,
+    # a ideia é retornar uma tupla em que:
+    #   - O primeiro item (seqComandos[0]) é o nome do processo
+    #   - O segundo (seqComandos[1]) é o comando a ser operado
+    #   - As demais variam dependendo do comando
+    #
+    # seqComandos tem esse formato: [('P3', 'C', '800'), ('P3', 'R', '100'), ('P3', 'P', '150'), ('P3', 'W', '110'), ..., ('P4', 'T')]
+    seqComandos = f.read()
+    seqComandos = [tuple(c.split(" ")) for c in seqComandos.split("\n")]
 
-    def __init__(self):
-        super().__init__()
+mp = MemoriaPrincipal(TAM_MEMORIA)
 
-        self.title("GMV - Trabalho de Sistemas Operacionais")
-        self.minsize(700,500)
-        self.maxsize(700,500)
-        self.configure(background= '#181f30') # Fundo da janela principal
+processosLista = []
+for com in seqComandos:
+    # TODO: Conforme o código crescer e tomar forma, adicionemos novos casos
+    # (possivelmente tornar esse if em um switch case com as instruções possíveis)
+    if com[1] == "C":
+        idProcesso = int(com[0][1])
+        tamProcesso = int(com[2])
+        try:
+            unidade = com[3]
+            match unidade:
+                case "KB":
+                    tamProcesso *= 2**10
+                case "MB":
+                    tamProcesso *= 2**20
+                case "GB":
+                    tamProcesso *= 2**30
+                case _:
+                    pass
+        except IndexError:
+            # Evitar erro no eventual caso de não passar unidade de tamanho
+            pass
+        processosLista.append(Processo(idProcesso, tamProcesso))
 
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-
-        # Inicializa o gerenciador de páginas
-        self.page_manager = PageManager(self) 
-
-        # Adiciona as páginas ao gerenciador
-        # Cada página recebe o 'self.page_manager' como seu 'controller'
-        self.page_manager.add_page(Tela_Inicial, "ui_pagina_inicial.py", controller=self.page_manager)
-        self.page_manager.add_page(Tela_Configurar, "ui_pagina_configurar.py", controller=self.page_manager)
-        self.page_manager.add_page(Tela_Input, "ui_pagina_input.py", controller= self.page_manager)
-        self.page_manager.add_page(Tela_Simular, "ui_pagina_simular.py", controller=self.page_manager)
-
-
-        # Exibe a página inicial ao iniciar
-        self.page_manager.show_page("ui_pagina_inicial.py")
-
-
-# --- Execução da Aplicação ---
-if __name__ == "__main__":
-    app = App()
-    app.mainloop()
+for p in processosLista:
+    print(p.id)
+    print(p.tamanho)
+    print(p.qtdPaginas)
