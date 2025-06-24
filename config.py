b548f4c24d8b70a2812937a66a6e055a591455d3
marianna lima <140180165+Limaniteroi@users.noreply.github.com>
2025-06-24 00:58:22 -0300

    Fiz com que as infos fossem da interface para o back

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
-from Interface import ui_pagina_configurar as ui_conf
-
 # Configurações do sistema de memória virtual
 # Estas configurações podem ser modificadas diretamente neste arquivo
 
 
-# Tamanho da TLB (número de entradas)
-TAMANHO_TLB = ui_conf.Tela_Configurar
+# Vinda das informações da interface
+TAM_MEM_PRINCIPAL = "1024"
+TAM_MEM_SECUNDARIA = "1024"
+TAM_PAGINA = "4"
+TAM_QUADRO = "4"
+TAM_END_LOGICO = "4"
+NUM_LINHAS_TLB = "4"
 
-
-# Tamanho da página em bytes (1MB)
-TAMANHO_PAGINA = 2**20
-
-# Tamanho total da memória física (1GB)
-TAMANHO_MEMORIA = 2**30
 
 # Função para verificar se um número é potência de 2
 def ehPotenciaDeDois(n):
@@ -26,15 +23,15 @@ def validarConfiguracoes():
     Valida se as configurações estão em valores razoáveis.
     Retorna True se tudo estiver ok, False caso contrário.
     """
-    if TAMANHO_TLB <= 0:
+    if int(NUM_LINHAS_TLB) <= 0:
         print("ERRO: TAMANHO_TLB deve ser maior que zero")
         return False
     
-    if TAMANHO_PAGINA <= 0 or not ehPotenciaDeDois(TAMANHO_PAGINA):  #Potência de 2
+    if int(TAM_PAGINA) <= 0 or not ehPotenciaDeDois(int(TAM_PAGINA)):  #Potência de 2
         print("ERRO: TAMANHO_PAGINA deve ser maior que zero e potencia de 2.")
         return False
     
-    if TAMANHO_MEMORIA <= 0 or TAMANHO_MEMORIA < TAMANHO_PAGINA:
+    if int(TAM_MEM_PRINCIPAL) <= 0 or int(TAM_MEM_PRINCIPAL) < int(TAM_PAGINA):
         print("ERRO: TAMANHO_MEMORIA deve ser maior que zero e maior que TAMANHO_PAGINA")
         return False
     
